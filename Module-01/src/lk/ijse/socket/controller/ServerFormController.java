6d9e48708d0958003dbbd168c606c241973a8cd8
AshanSandeep06 <ashansandeep06@gmail.com>
2022-08-08 18:28:42 +0530

    Create Server Form

--- Module-01/src/lk/ijse/socket/controller/ServerFormController.java
+++ Module-01/src/lk/ijse/socket/controller/ServerFormController.java
@@ -1,5 +1,8 @@
 package lk.ijse.socket.controller;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
 /**
  * @author : Ashan Sandeep
  * @since : 0.1.0
@@ -7,4 +10,6 @@ package lk.ijse.socket.controller;
 
 public class ServerFormController {
 
+    public ScrollPane scrollPane;
+    public VBox vbox;
 }
