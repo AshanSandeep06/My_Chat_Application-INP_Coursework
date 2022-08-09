92bd111d8ae8e60e0f7290d55e0fdd3ca0f96462
AshanSandeep06 <ashansandeep06@gmail.com>
2022-08-09 17:45:52 +0530

    Refactored ClientFormController

--- Module-01/src/lk/ijse/socket/controller/ClientFormController.java
+++ Module-01/src/lk/ijse/socket/controller/ClientFormController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.socket.util.Delta;
+import lk.ijse.socket.util.OptionUtil;
 
 /**
  * @author : Ashan Sandeep
@@ -25,33 +28,51 @@ public class ClientFormController {
     public ImageView imgCameraIcon;
     public ImageView imgEmojiIcon;
 
+    final Delta dragDelta = new Delta();
+
     public void mouseDraggedOnAction(MouseEvent mouseEvent) {
+        Stage window = (Stage) mainContext.getScene().getWindow();
+        window.setX(mouseEvent.getScreenX() + dragDelta.x);
+        window.setY(mouseEvent.getScreenY() + dragDelta.y);
     }
 
     public void mousePressedOnAction(MouseEvent mouseEvent) {
-    }
-
-    public void closeOnAction(MouseEvent mouseEvent) {
+        Stage window = (Stage) mainContext.getScene().getWindow();
+        dragDelta.x = window.getX() - mouseEvent.getScreenX();
+        dragDelta.y = window.getY() - mouseEvent.getScreenY();
     }
 
     public void closeMouseEnteredOnAction(MouseEvent mouseEvent) {
-    }
-
-    public void minimizeOnAction(MouseEvent mouseEvent) {
-    }
-
-    public void minimizeMouseEnteredOnAction(MouseEvent mouseEvent) {
+        OptionUtil.closeMouseEnter(closeLabel, imgClose);
     }
 
     public void closeMouseExitedOnAction(MouseEvent mouseEvent) {
+        OptionUtil.closeMouseExit(closeLabel, imgClose);
+    }
+
+    public void minimizeMouseEnteredOnAction(MouseEvent mouseEvent) {
+        OptionUtil.minimizeMouseEnter(minimizeLable);
     }
 
     public void minimizeMouseExitedOnAction(MouseEvent mouseEvent) {
+        OptionUtil.minimizeMouseExit(minimizeLable);
+    }
+
+    public void minimizeOnAction(MouseEvent mouseEvent) {
+        Stage stage = (Stage) context.getScene().getWindow();
+        OptionUtil.minimizeOnAction(stage);
+    }
+
+    public void closeOnAction(MouseEvent mouseEvent) {
+        Stage stage = (Stage) context.getScene().getWindow();
+        OptionUtil.closeOnAction(stage);
     }
 
     public void txtMessagesSendOnAction(ActionEvent event) {
+
     }
 
     public void messagesSendOnAction(ActionEvent event) {
+
     }
 }
