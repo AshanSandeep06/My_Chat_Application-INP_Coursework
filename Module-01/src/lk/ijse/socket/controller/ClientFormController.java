013832ccc235fe6f6b5a454be1802aa37cd981be
AshanSandeep06 <ashansandeep06@gmail.com>
2022-08-09 16:03:31 +0530

    Refactored all attributes and events to ClientFormController class

--- Module-01/src/lk/ijse/socket/controller/ClientFormController.java
+++ Module-01/src/lk/ijse/socket/controller/ClientFormController.java
@@ -1,5 +1,13 @@
 package lk.ijse.socket.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * @author : Ashan Sandeep
  * @since : 0.1.0
@@ -7,4 +15,44 @@ package lk.ijse.socket.controller;
 
 public class ClientFormController {
 
+    public AnchorPane mainContext;
+    public AnchorPane context;
+    public Label closeLabel;
+    public ImageView imgClose;
+    public Label minimizeLable;
+    public Label lblClientName;
+    public TextArea txtMessageArea;
+    public TextField txtMessage;
+    public ImageView imgCameraIcon;
+    public ImageView imgEmojiIcon;
+
+    public void mouseDraggedOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void mousePressedOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void closeMouseEnteredOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void minimizeOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void minimizeMouseEnteredOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void closeMouseExitedOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void minimizeMouseExitedOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void txtMessagesSendOnAction(ActionEvent event) {
+    }
+
+    public void messagesSendOnAction(ActionEvent event) {
+    }
 }
